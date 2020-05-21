391a8a24dc8d7017127f3f7f9f12356d983291cc
DarkC <k.haunschmied@edumail.at>
2020-05-21 17:36:28 +0200

    add netlifycms plugin and config

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,6 +77,12 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+        base_url: process.env.BACKEND_BASE_URL
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
