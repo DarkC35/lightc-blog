4ee7547c3983afa947a857869b6b718f6b5cfabb
DarkC <k.haunschmied@edumail.at>
2020-06-10 19:59:28 +0200

    update license and copyright

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ React Gatsby Blog with Netlify CMS based on the [gastby-starter-blog template](h
 Additional installed plugins:
 
 * [gatsby-plugin-netlify-cms](https://github.com/gatsbyjs/gatsby/tree/master/packages/gatsby-plugin-netlify-cms)
+
+## License
+
+Source code is licensed under the MIT license.
+
+Contents of this blog are Copyright &copy; 2020 DarkC35. All rights reserved.
--- src/components/layout.js
+++ src/components/layout.js
@@ -59,7 +59,7 @@ const Layout = ({ location, title, children }) => {
       <header>{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
+        © {new Date().getFullYear()} DarkC, Built with
         {` `}
         <a href="https://www.gatsbyjs.org">Gatsby</a>
         , Check out the <a href="https://github.com/DarkC35/lightc-blog">Source Code</a>
